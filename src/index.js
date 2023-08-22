ea4c020cd91fb6c2d35fe84d56bfe61a2cc0037d
Sean Corbett <stcorbett@gmail.com>
2023-08-21 22:31:47 -0500

    timeilne pages

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import { Application } from "../node_modules/@hotwired/stimulus/dist/stimulus.js"
+
+// import RevealController from "./js/reveal_with_outside_controller.js"
+
+window.Stimulus = Application.start()
+
+// Stimulus.register("reveal", RevealController)
